946d1e1ae62e64ac23ccba092a45708e88b02449
Patryk <patrykr124@gmail.com>
2025-05-31 13:14:01 +0200

    modified:   client/src/components/layout/services/BoxServices.tsx

--- client/src/components/layout/services/BoxServices.tsx
+++ client/src/components/layout/services/BoxServices.tsx
@@ -10,15 +10,15 @@ export default function BoxServices({id,category,img,title,packages,postedBy } :
     >
       <div className="top">
         <img
-          src={`http://localhost:3000/uploads/${img}`}
+          src={`${import.meta.env.VITE_API_URL}/uploads/${img}`}
           className="w-full h-[180px]  object-cover object-center"
           alt="bg_img"
         />
-        
+
       </div>
       <div className="bg-black/5 h-full p-2 space-y-2 justify-center flex flex-col">
         <div className="flex gap-2 items-center">
-          <img src={`http://localhost:3000/${postedBy.avatarUrl}`} className="w-6 h-6 rounded-full bg-black/20" alt="" />
+          <img src={`${import.meta.env.VITE_API_URL}/${postedBy.avatarUrl}`} className="w-6 h-6 rounded-full bg-black/20" alt="" />
           <div className="flex gap-1">
             <p className="">By</p>
             <p className="font-semibold"> {postedBy.name}</p>
